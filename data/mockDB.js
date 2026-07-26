0ba8e65427cfe2adadb36e2d4b28264cc409ba00
vivekyadav1220 <vivekdor@gmail.com>
2026-07-26 19:31:56 +0530

    Initialize mock database with empty arrays

---
+++ data/mockDB.js
@@ -0,0 +1,15 @@
+const users = [];
+const recipes = [];
+const categories = [];
+const ratings = [];
+const comments = [];
+const favorites = [];
+
+module.exports = {
+    users,
+    recipes,
+    categories,
+    ratings,
+    comments,
+    favorites
+};
